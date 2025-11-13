d5e8a2c5d9717ebbd8c2affa03a201c98207d5ff
bardia <bardiakzm@gmail.com>
2025-11-13 20:55:50 +0100

    init: spring boot in auth-service

--- services/auth-service/build.gradle.kts
+++ services/auth-service/build.gradle.kts
@@ -1,25 +1,27 @@
 plugins {
     id("java")
-    id("application")
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.github.bardiakz"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
-application {
-    mainClass = "io.github.bardiakz.Main"
-}
-
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
