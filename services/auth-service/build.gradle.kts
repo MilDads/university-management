f985e481105bbef8cf0c10e1d233991e3d383f55
bardia <bardiakzm@gmail.com>
2025-12-07 16:55:22 +0100

    init: docker-compose

--- services/auth-service/build.gradle.kts
+++ services/auth-service/build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
