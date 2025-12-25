3469c5affe3276d5340498a4d3f33b7f47d3435a
bardia <bardiakzm@gmail.com>
2025-12-25 14:23:32 +0100

    add: models to marketplace

---
+++ services/marketplace-service/src/main/java/io/github/bardiakz/marketplace_service/model/ProductCategory.java
@@ -0,0 +1,18 @@
+package io.github.bardiakz.marketplace_service.model;
+
+public enum ProductCategory {
+    WORKSHOP_TICKET,
+    EVENT_TICKET,
+    BOOK,
+    MERCHANDISE,
+    SERVICE,
+    OTHER
+}
+
+enum OrderStatus {
+    PENDING,
+    PAYMENT_PENDING,
+    COMPLETED,
+    FAILED,
+    CANCELLED
+}
