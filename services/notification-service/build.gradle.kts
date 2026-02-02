b7fab84ff1200dbf2911c71fe5397f227562988a
bardia <bardiakzm@gmail.com>
2026-02-02 20:49:12 +0100

    lazyCommit

--- services/notification-service/build.gradle.kts
+++ services/notification-service/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     // PostgreSQL Driver
     runtimeOnly("org.postgresql:postgresql")
